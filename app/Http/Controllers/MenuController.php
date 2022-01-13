180dc411138eb2dd9b8823a1f51cbe911e7e48c5
Tom Keijzers <PS220823@summacollege.nl>
2022-01-13 10:17:58 +0100

    13/1/2022, Inzien welke ingredienten bij welke pizza horen

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -14,16 +14,20 @@ class MenuController extends Controller
     public function index()
     {
         $menus = Menu::all();
-
         return view('menus.index',compact('menus'));
     }
-    public function edit() 
+    public function edit($menu)
     {
         //Dit is niet de EDIT van de menu tabel! Met deze functie kun je ingredienten aan pizza's toevoegen of van pizza's afhalen.
         $ingredienten = Ingredient::all();
+        $menu = Menu::find($menu);
+        foreach ($menu->ingredients as $ingredients) {
+            echo $ingredients['ingredient'];
+        }
 
-        
-        return view('menus.edit',compact('ingredienten'));
+
+
+        //return view('menus.edit',compact('ingredienten'));
 
         //wordt vervolgt
 
